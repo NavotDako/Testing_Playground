903c6c3a4a9ac41997973ce09a08fb1c424a906a
udi.valer <udi.valer@experitest.com>
2016-08-30 13:20:58 +0300

    Udi first commit

--- src/Tests/TenFreeApps.java
+++ src/Tests/TenFreeApps.java
@@ -18,25 +18,33 @@ public class TenFreeApps extends AbsTest {
     protected void AndroidRunTest() {
 
         client.launch("com.android.vending/.AssetBrowserActivity", false, true);
+        client.sync(1500 , 0 , 10000);
+        if(client.isElementFound("NATIVE" , "xpath=//*[@text='ACCEPT']" , 0))
+            client.click("NATIVE" , "xpath=//*[@text='ACCEPT']" , 0 , 1);
+        client.sync(1500 , 0 , 10000);
+        if(client.isElementFound("NATIVE" ,"xpath=//*[@text='TOP CHARTS']" , 0))
+            client.click("NATIVE" ,"xpath=//*[@text='TOP CHARTS']" , 0 , 1);
         client.click("NATIVE" , "xpath=//*[@text='TOP FREE']" , 0 , 1);
 
         int countOfOnScreenApps;
+        int counterOfEnteredApp = 0;
         ArrayList<String> freeApps = new ArrayList<>();
 
         while(freeApps.size()<10) {
             countOfOnScreenApps = client.getElementCount("NATIVE", "xpath=//*[@id='play_card']/*[@id='li_title' and @onScreen='true']");
             for (int i = 0; i < countOfOnScreenApps; i++) {
                 String temp = client.elementGetText("NATIVE", "xpath=//*[@id='play_card']/*[@id='li_title' and @onScreen='true']", i);
-                freeApps.add(temp);
+                if(Character.getNumericValue(temp.charAt(0) - 1) == counterOfEnteredApp || (Character.getNumericValue(temp.charAt(0) - 1) == 0 && counterOfEnteredApp == 9)){
+                    freeApps.add(temp);
+                    counterOfEnteredApp++;
+                }
             }
             client.swipeWhileNotFound("Down" , 200 , 3500 , "NATIVE" , "xpath=//*[@id='play_card']/*[@id='li_title' and @onScreen='true' and contains(@text , '" + countOfOnScreenApps+1 + "')]" , 0 , 1000 , 1 , false);
         }
 
-        ArrayList<String> freeAppsNoDuplicate = removeDuplicate(freeApps);
-
         System.out.println("------------------- 10's free apps today ------------------");
         for(int i=0 ; i<10 ; i++) {
-            System.out.println(freeAppsNoDuplicate.get(i));
+            System.out.println(freeApps.get(i));
         }
         System.out.println("-----------------------------------------------------------");
 
@@ -48,24 +56,32 @@ public class TenFreeApps extends AbsTest {
     @Override
     protected void IOSRunTest() {
 
-        client.click("NATIVE" , "xpath=//*[@text='App Store']" , 0 , 1);
-        client.click("NATIVE" , "xpath=//*[@text='Top Charts']" , 0 , 1);
-        client.click("NATIVE" , "xpath=//*[@text='Free']" , 0 , 1);
+        client.launch("com.apple.AppStore", false, true);
+        client.sync(1000 , 0 , 10000);
+        client.click("NATIVE" , "xpath=//*[@text='Top Charts']" , 0 , 2);
+        if(client.isElementFound("NATIVE" , "xpath=//*[@text='Free' and @knownSuperClass='UIButton']" , 0))
+            client.click("NATIVE" , "xpath=//*[@text='Free' and @knownSuperClass='UIButton']" , 0 , 1);
+        else
+            client.click("NATIVE" , "xpath=//*[@text='Free' and @knownSuperClass='UISegment']" , 0 , 1);
+
         int countOfOnScreenApps;
         ArrayList<String> freeApps = new ArrayList<>();
 
         while(freeApps.size()<10) {
-            countOfOnScreenApps = client.getElementCount("NATIVE", "//*[@text = 'GET' or @text= 'Download' and @onScreen='true']/../../../*[@x=0]");
+            countOfOnScreenApps = client.getElementCount("NATIVE", "//*[@class='UIAView' and @knownSuperClass='UIView' and @enabled='true' and @y>0 and @width>0 and @height>0 and @hidden='false' and @onScreen='true']/../*/*/*[@text='GET' or @text='Download']/../../../*[contains(@text , ',')]");
             for (int i = 0; i < countOfOnScreenApps; i++) {
-                    String temp = client.elementGetText("NATIVE", "//*[@text = 'GET' or @text= 'Download' and @onScreen='true']/../../../*[@x=0]", i);
-                    freeApps.add(temp);
+                    String temp = client.elementGetText("NATIVE", "//*[@class='UIAView' and @knownSuperClass='UIView' and @enabled='true' and @y>0 and @width>0 and @height>0 and @hidden='false' and @onScreen='true']/../*/*/*[@text='GET' or @text='Download']/../../../*[contains(@text , ',')]", i);
+                    if(!freeApps.contains(temp))
+                        freeApps.add(temp);
             }
-            client.swipeWhileNotFound("Down" , 200 , 3500 , "NATIVE" , "//*[@text = 'GET' or @text= 'Download' and @onScreen='true']/../../../*[@x=0 and contains(@text , '"+ countOfOnScreenApps+ "']" , 0 , 1000 , 1 , false);
+            if(client.isElementFound("NATIVE" , "xpath=//*[@text='Free' and ./*[@class='UIAView']]" , 0))
+                client.elementSwipe("NATIVE", "xpath=//*[@text='Free' and ./*[@class='UIAView']]", 0 , "Down", 200 , 3500);
+            else
+                client.swipeWhileNotFound("Down" , 200 , 3500 , "NATIVE" , "//*[@class='UIAView' and @knownSuperClass='UIView' and @enabled='true' and @y>0 and @width>0 and @height>0 and @hidden='false' and @onScreen='true']/../*/*/*[@text='GET' or @text='Download']/../../../*[contains(@text , '"+ (countOfOnScreenApps+1) + ",')]" , 0 , 1000 , 1 , false);
         }
 
-        ArrayList<String> freeAppsNoDuplicate = removeDuplicate(freeApps);
         ArrayList<String> toPrint = new ArrayList<>();
-        for(String temp:freeAppsNoDuplicate){
+        for(String temp:freeApps){
             String[] toCut = temp.split(",");
             String toAdd = toCut[1];
             toPrint.add(toAdd);
@@ -81,16 +97,6 @@ public class TenFreeApps extends AbsTest {
         client.applicationClearData(appNAme);
         client.applicationClose(appNAme);
 
-    }
-
-    private static ArrayList<String> removeDuplicate(ArrayList<String> arr){
-        Object[] st = arr.toArray();
-        for (Object s : st) {
-            if (arr.indexOf(s) != arr.lastIndexOf(s)) {
-                arr.remove(arr.lastIndexOf(s));
-            }
-        }
-        return arr;
     }
 
 }
